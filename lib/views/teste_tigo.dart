d78d3d86350d579b669576b1bdbfa7803b1a774a
Thiago Vinícius Braga Aksenen <aksenen1.thiago@gmail.com>
2025-11-01 11:24:13 -0300

    Evento em breve adicionado

--- lib/views/teste_tigo.dart
+++ lib/views/teste_tigo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:role_curitiba/componentes/evento_comp.dart';
+import 'package:role_curitiba/componentes/evento_em_breve_comp.dart';
 
 class TesteTigo extends StatelessWidget {
   const TesteTigo({super.key});
@@ -11,6 +12,7 @@ class TesteTigo extends StatelessWidget {
         color: const Color.fromARGB(14, 0, 0, 0),
         child: Column(
           children: [
+            Text("Cards Padrão"),
             SizedBox(
               height: 240,
               child: ListView.builder(
@@ -18,6 +20,22 @@ class TesteTigo extends StatelessWidget {
                 itemCount: 5,
                 scrollDirection: Axis.horizontal,
               ),
+            ),
+            Text("Cards em breve"),
+            Expanded(child:
+            SizedBox(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) => EventoEmBreveComp(),
+                itemCount: 6,
+                padding: EdgeInsets.all(10),
+                scrollDirection: Axis.vertical,
+              ),
+            ),
             ),
           ],
         ),
